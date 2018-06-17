206c67cd3c2d0b808eb38ea5cefb9d97d4629a98
vishaljangid02 <40339791+vishaljangid02@users.noreply.github.com>
2018-06-17 22:19:47 +0530

    farzi line
    edit ki h

---
+++ Area.java
@@ -0,0 +1,22 @@
+class Room
+{
+int length;
+int height;
+}
+class Area
+{
+public static void main(String args[])
+{
+Room r=new Room();
+r.length=10;
+r.height=20;
+System.out.println("area is:"+(r.length*r.height));
+
+Room r1=new Room();
+r1.length=20;
+r1.height=30;
+System.out.println("area is:"+(r1.length*r1.height));
+}
+}
+
+djgjsaksakdk
